f6a93ed2144a461697a4ad8696e2a4d04ff68944
Dylan-Wheeler <dylan.wheeler1@ucalgary.ca>
2020-02-15 17:29:43 -0700

    small changes

--- app/src/main/java/com/example/hackathon2020/MyAdapter.java
+++ app/src/main/java/com/example/hackathon2020/MyAdapter.java
@@ -1,6 +1,8 @@
 package com.example.hackathon2020;
 
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,13 +20,42 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         }
     }
 
-    class ViewHolder2 extends RecyclerView.ViewHolder {
+    class ViewHolder1 extends RecyclerView.ViewHolder {
         TextView bathroomName;
 
-        public ViewHolder2(TextView textView) {
+        public ViewHolder1(TextView textView) {
             super(textView);
 
             bathroomName = textView;
+        }
+    }
+
+    class ViewHolder2 extends RecyclerView.ViewHolder {
+        TextView bathroomDistance;
+
+        public ViewHolder2(TextView textView){
+            super(textView);
+
+            bathroomDistance = textView;
+        }
+    }
+
+    class ViewHolder3 extends RecyclerView.ViewHolder {
+        RatingBar bathroomRating;
+
+        public ViewHolder3(RatingBar ratingView) {
+            super(ratingView);
+
+            bathroomRating = ratingView;
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case 0: return new ViewHolder0();
+            case 2: return new ViewHolder2(...);
+             ...
         }
     }
 
